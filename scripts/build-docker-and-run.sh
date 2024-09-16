4e9295a318e972f3e92bd33d97ffdd92ea15da73
henryzhuhr <henry.zhuhr@icloud.com>
2024-09-16 15:27:39 +0800

    🐳 Dockerfile: 跨平台架构构建

--- scripts/build-docker-and-run.sh
+++ scripts/build-docker-and-run.sh
@@ -1,13 +1,14 @@
 IMAGE_TAG="ubuntu:$(date +%Y-%m%d-%H%M%S)"
 
 ARG_NOCACHE="--no-cache"
-# ARG_PROGRESS="--progress=plain" 
+# ARG_PROGRESS="--progress=plain"
+# -- Multi-platform builds: https://docs.docker.com/build/building/multi-platform/
+# ARG_PLATFORM="--platform=linux/amd64"
+# ARG_PLATFORM="--platform=linux/arm64"
 docker build \
     --tag $IMAGE_TAG \
     --file dockerfiles/Dockerfile \
-    $ARG_NOCACHE \
-    $ARG_PROGRESS \
-    .
+    $ARG_NOCACHE $ARG_PROGRESS $ARG_PLATFORM .
 
 docker run \
     --interactive --tty \
