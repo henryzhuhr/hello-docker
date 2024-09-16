2189621615acb4f20f7cad0b0d08240b67256c7f
henryzhuhr <henry.zhuhr@icloud.com>
2024-09-16 12:12:29 +0800

    🐳 Dockerfile: 换源，安装软件

---
+++ scripts/build-docker-and-exec-in.sh
@@ -0,0 +1,20 @@
+
+IMAGE_TAG="ubuntu:$(date +%Y-%m%d-%H%M%S)"
+
+ARG_NOCACHE="--no-cache"
+# ARG_PROGRESS="--progress=plain" 
+docker build \
+    --tag $IMAGE_TAG \
+    --file dockerfiles/Dockerfile \
+    $ARG_NOCACHE \
+    $ARG_PROGRESS \
+    .
+
+docker run \
+    --interactive --tty \
+    --rm $IMAGE_TAG \
+    /bin/bash
+
+# -i --interactive: Keep STDIN open even if not attached
+# -t --tty: Allocate a pseudo-TTY
+# --rm: Automatically remove the container and its associated anonymous volumes when it exits
