63ade33c76d84d5217359a0e5f3fb8821510e4a1
henryzhuhr <henry.zhuhr@icloud.com>
2024-09-18 00:16:35 +0800

    🐳 Dockerfile: 完善构建ubuntu系统

--- scripts/build-docker-and-run.sh
+++ scripts/build-docker-and-run.sh
@@ -1,6 +1,6 @@
 IMAGE_TAG="ubuntu:$(date +%Y-%m%d-%H%M%S)"
 
-ARG_NOCACHE="--no-cache"
+# ARG_NOCACHE="--no-cache"
 # ARG_PROGRESS="--progress=plain"
 # -- Multi-platform builds: https://docs.docker.com/build/building/multi-platform/
 # ARG_PLATFORM="--platform=linux/amd64"
@@ -10,11 +10,10 @@ docker build \
     --file dockerfiles/Dockerfile \
     $ARG_NOCACHE $ARG_PROGRESS $ARG_PLATFORM .
 
-docker run \
-    --interactive --tty \
-    --rm $IMAGE_TAG \
-    /bin/bash
+docker run -it --rm $IMAGE_TAG /bin/bash --login
 
+# docker run [OPTIONS]  IMAGE        [COMMAND] [ARG...]
+# docker run -it --rm   $IMAGE_TAG   /bin/bash  --login
 # -i --interactive: Keep STDIN open even if not attached
 # -t --tty: Allocate a pseudo-TTY
 # --rm: Automatically remove the container and its associated anonymous volumes when it exits
