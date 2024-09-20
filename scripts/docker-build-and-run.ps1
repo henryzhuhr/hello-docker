81c48bda236bb5810a3c013c874f2f7503dc7939
henryzhuhr <henry.zhuhr@icloud.com>
2024-09-20 23:50:55 +0800

    🐳 Dockerfile: powershell 脚本适配

--- scripts/docker-build-and-run.ps1
+++ scripts/docker-build-and-run.ps1
@@ -7,7 +7,8 @@ $IMAGE_TAG = "ubuntu:$($TAG)"
 $buildArgs = @(
     # "--no-cache",  # 如果不需要缓存，请取消注释
     # "--progress=plain",  # 如果需要简单的进度输出，请取消注释
-    # "--platform", "linux/arm64"  # 如果需要指定平台，请取消注释
+    # "--platform", "linux/arm64",  # 如果需要指定平台，请取消注释
+    ""
 )
 docker build . -t $IMAGE_TAG -f dockerfiles\Dockerfile $buildArgs
 
@@ -19,11 +20,12 @@ $USER_NAME = "root"
 $WORKDIR = "/$USER_NAME/$PROJECT_NAME"
 # 定义运行参数（根据需要取消注释）
 $buildArgs = @(
-    "-i"    # -i --interactive: Keep STDIN open even if not attached
-    "-t"    # -t --tty: Allocate a pseudo-TTY
+    "-i",    # -i --interactive: Keep STDIN open even if not attached
+    "-t",    # -t --tty: Allocate a pseudo-TTY
     "--rm"  # --rm: Automatically remove the container and its associated anonymous volumes when it exits
-    "-v" "$PWD:$WORKDIR" # 挂载当前目录
-    "-w" "$WORKDIR"      # 指定工作目录
-    "-u" "$USER_NAME"    # 指定用户
+    "-v", "${PWD}:$WORKDIR", # 挂载当前目录
+    "-w", "$WORKDIR",      # 指定工作目录
+    "-u", "$USER_NAME",    # 指定用户
+    ""
 )
 docker run $buildArgs $IMAGE_TAG /bin/bash --login
