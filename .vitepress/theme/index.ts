1f460f6e5a95f1d18f44cd7760781c3752bb4350
henryzhuhr <henry.zhuhr@icloud.com>
2024-10-11 21:54:54 +0800

    🎨 theme: 优化图片查看插件配置

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,22 +1,37 @@
-/**
- * 自定义主题: https://vitepress.dev/guide/custom-theme
- */
+// https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
+import { onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import Layout from "./Layout.vue";
+import mediumZoom from 'medium-zoom'
 import './style.scss'
-
 
 export default {
   extends: DefaultTheme,
-  Layout: Layout,
-  // Layout: () => {
-  //   return h(DefaultTheme.Layout, null, {
-  //     // https://vitepress.dev/guide/extending-default-theme#layout-slots
-  //   })
-  // },
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
+  },
   enhanceApp({ app, router, siteData }) {
     // ...
+  },
+
+  setup() {
+    const route = useRoute()
+    const initZoom = () => {
+      // 为所有图片增加缩放功能
+      // https://welives.github.io/blog/misc/vitepress/image-zoom.html
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
+    }
+    onMounted(() => {
+      initZoom()
+    })
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
   }
+
 } satisfies Theme
