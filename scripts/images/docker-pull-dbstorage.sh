08fda41b95c4bb725d7d4bb520e6f2cc0fe28450
henryzhuhr <henry.zhuhr@icloud.com>
2025-02-10 14:09:25 +0800

    🐳 Docker: 添加多个数据库和操作系统的镜像拉取脚本

---
+++ scripts/images/docker-pull-dbstorage.sh
@@ -0,0 +1,27 @@
+# mysql: https://hub.docker.com/_/mysql
+docker pull mysql
+docker pull mysql:9
+
+# redis: https://hub.docker.com/_/redis
+docker pull redis
+docker pull redis:7
+
+# mongo: https://hub.docker.com/_/mongo
+docker pull mongo
+docker pull mongo:8.0
+
+# kafka: https://hub.docker.com/r/bitnami/kafka
+docker pull bitnami/kafka
+docker pull bitnami/kafka:3.9
+
+# etcd: https://hub.docker.com/r/bitnami/etcd
+docker pull bitnami/etcd
+docker pull bitnami/etcd:3.5
+
+# elasticsearch: https://hub.docker.com/_/elasticsearch
+docker pull elasticsearch
+docker pull elasticsearch:8.17.1
+
+# clickhouse: https://hub.docker.com/r/bitnami/clickhouse
+docker pull bitnami/clickhouse
+docker pull bitnami/clickhouse:25
